9c640dc4679c540f26e1634ff8a8c43cb9e415d4
kobarei <koba.reiji@gmail.com>
2014-06-24 03:59:18 +0900

    add diagram in doc

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,5 +1,9 @@
 class Library < ActiveRecord::Base
 
+  has_many :users
+  has_many :staffs
+  has_many :publications
+
   validates :name, presence: true
   validates :subdomain, presence: true, uniqueness: true
 
