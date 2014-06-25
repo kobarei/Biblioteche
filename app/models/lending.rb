f53384bc28c8205944712dd1604fa5a9f5a14d3e
kobarei <koba.reiji@gmail.com>
2014-06-25 17:01:34 +0900

    move common methods of lending and reservation model to module

--- app/models/lending.rb
+++ app/models/lending.rb
@@ -1,27 +1,11 @@
 class Lending < ActiveRecord::Base
   include Biblio::LendingReservation
 
-  belongs_to :user
-  belongs_to :book
-  belongs_to :magazine
-
   validate on: :create do
-    errors.add(:base, "年齢制限がかかっています")       unless pass_age_limit?
-    errors.add(:base, "不正な蔵書です")               unless proper_library?
     errors.add(:base, "借りるには予約する必要があります") unless no_need_reservation?
-    errors.add(:base, "すでにレンタルしています")       unless no_publication_lending?
-  end
-
-  scope :alive, -> { where expired_at: nil }
-  scope :expired, -> { where.not expired_at: nil }
-
-  before_create do
-    self.expire_at = 7.days.since.end_of_day
   end
 
   after_create do
-    publication.remain -= 1
-    publication.update_status
     reservation = Reservation.alive.user_publication(user, publication)
     reservation.destroy if reservation.present?
   end
--- lib/biblio/lending_reservation.rb
+++ lib/biblio/lending_reservation.rb
@@ -3,6 +3,31 @@ module Biblio
 
     def self.included(base)
       base.extend(ClassMethods)
+
+      base.class_eval {
+        belongs_to :user
+        belongs_to :book
+        belongs_to :magazine
+
+        validate on: :create do
+          errors.add(:base, "年齢制限がかかっています") unless pass_age_limit?
+          errors.add(:base, "不正な蔵書です")         unless proper_library?
+          errors.add(:base, "すでにレンタルしています") unless no_publication_lending?
+        end
+
+        scope :alive, -> { where expired_at: nil }
+        scope :expired, -> { where.not expired_at: nil }
+
+        before_create do
+          self.expire_at = 7.days.since.end_of_day
+        end
+
+        after_create do
+          publication.remain -= 1
+          publication.update_status
+        end
+
+      }
     end
 
     module ClassMethods
